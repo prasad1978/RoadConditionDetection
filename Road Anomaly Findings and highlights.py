904a8392767058e49d63acdfc31d1bd048162f77
prasad1978 <cheeran.prasad@gmail.com>
2021-05-06 12:03:21 +0530

    Add files via upload

---
+++ Road Anomaly Findings and highlights.py
@@ -0,0 +1,4 @@
+#analyze different multiclass supervised
+#machine learning techniques to effectively classify road surface
+#conditions using accelerometer, gyroscope and GPS data collected
+#from smartphones
